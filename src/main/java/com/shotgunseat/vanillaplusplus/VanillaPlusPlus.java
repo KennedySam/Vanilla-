98ed85fd424c9c90fae67fcfe89975b57bbaaa40
SamJamesKennedy <samjameskennedy@gmail.com>
2018-09-05 21:31:41 +0100

    More setup

--- src/main/java/com/shotgunseat/vanillaplusplus/VanillaPlusPlus.java
+++ src/main/java/com/shotgunseat/vanillaplusplus/VanillaPlusPlus.java
@@ -1,18 +1,40 @@
 package com.shotgunseat.vanillaplusplus;
 
+import com.shotgunseat.vanillaplusplus.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = VanillaPlusPlus.MODID, version = VanillaPlusPlus.VERSION)
+@Mod(modid = VanillaPlusPlus.MODID, version = VanillaPlusPlus.VERSION, name = VanillaPlusPlus.NAME)
 public class VanillaPlusPlus {
 
-    public static final String MODID = "examplemod";
+    public static final String MODID = "vanillaplusplus";
     public static final String VERSION = "1.0";
     public static final String NAME = "Vanilla++";
 
+    @SidedProxy(clientSide = "com.shotgunseat.vanillaplusplus.proxy.ClientProxy", serverSide = "com.shotgunseat.vanillaplusplus.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static VanillaPlusPlus instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+        proxy.preInit(event);
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
     }
     
 }
